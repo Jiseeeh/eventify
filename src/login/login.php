363b0627583d00428cb8649849e49a366f8d9b5d
Jiseeeh <johncarlo.camara1@gmail.com>
2023-10-09 00:38:10 +0800

    feat: add login/signup page

---
+++ src/login/login.php
@@ -0,0 +1,51 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link rel="stylesheet" href="/global.css">
+    <link rel="stylesheet" href="login.css">
+</head>
+
+<body>
+    <main class="sections-wrapper">
+        <section class="left-section">
+            <img src="/public/eventify-clear.png" alt="" class="left-section__img">
+            <h1 class="left-section__heading">Login to your Account</h1>
+            <form action="" class="left-section__form">
+                <label for="username">Username</label>
+                <input id="username" name="username" type="text">
+                <label for="password">Password</label>
+                <input id="password" name="password" type="password">
+                <span>Don&apos;t have an account? <a class="left-section__form__login-link" href="/src/sign-up/signup.php">Sign up</a></span>
+                <button class="left-section__form__btn" type="submit">Login</button>
+            </form>
+        </section>
+        <section class="right-section">
+            <section class="right-section__content">
+                <h1 class="right-section__content__heading">Welcome to the Underworld</h1>
+                <p class="right-section__content__description">Zombie ipsum brains reversus ab cerebellum viral inferno,
+                    brein nam rick mend grimes malum cerveau cerebro. De carne cerebro lumbering animata cervello corpora
+                    quaeritis. Summus thalamus brains sit​​, morbo basal ganglia vel maleficia? De braaaiiiins apocalypsi
+                    gorger omero prefrontal cortex undead survivor fornix dictum mauris. Hi brains mindless mortuis limbic
+                    cortex soulless creaturas optic nerve, imo evil braaiinns stalking monstra hypothalamus adventus resi
+                    hippocampus dentevil vultus brain comedat cerebella pitiutary gland viventium. Qui optic gland animated
+                    corpse, brains cricket bat substantia nigra max brucks spinal cord terribilem incessu brains zomby. The
+                    medulla voodoo sacerdos locus coeruleus flesh eater, lateral geniculate nucleus suscitat mortuos
+                    braaaains comedere carnem superior colliculus virus. Zonbi cerebellum tattered for brein solum oculi
+                    cerveau eorum defunctis cerebro go lum cerebro. Nescio brains an Undead cervello zombies. Sicut thalamus
+                    malus putrid brains voodoo horror. Nigh basal ganglia tofth eliv ingdead.
+
+                </p>
+            </section>
+        </section>
+    </main>
+</body>
+
+</html>
